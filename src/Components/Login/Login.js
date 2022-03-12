de36c00da3e36c55315b9121466f80e83a69fcea
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-03-12 17:44:43 +0600

    Add login Form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <p>login Page</p>
+        <div className="Login-banner-image">
+            <div className="container">
+                <form >
+                    <div className="form-group mt-5">
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name" />
+                    </div>
+                    <div className="form-group mt-3">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                    </div>
+                    <div className="form-group mt-3">
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    </div>
+                    <div className="form-group mt-3">
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirm Password" />
+                    </div>
+                   
+
+                    <button type="submit" className="btn btn-primary mt-3">Submit</button>
+                </form>
+            </div>
         </div>
     );
 };
