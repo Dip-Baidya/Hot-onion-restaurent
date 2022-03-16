62b20ac84b6997ddf587dfa2c2316373aeb97a14
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-03-16 18:32:44 +0600

    Add Choose Us Section

---
+++ src/Components/ChooseUs/ChooseUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChooseUs = () => {
+    return (
+        <div className="container">
+            <h1>Choose Us Section</h1>
+        </div>
+    );
+};
+
+export default ChooseUs;
